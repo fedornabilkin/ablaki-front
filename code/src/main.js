1e7892f644f27999616fff71d1a6d38a2292d121
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-11-28 23:34:18 +0300

    update app init

--- code/src/main.js
+++ code/src/main.js
@@ -1,20 +1,27 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
+import Vue, { createApp } from 'vue';
+import { BootstrapVue3 } from './plugins/bootstrap-vue';
 import './plugins/axios'
 import App from './App.vue'
-import store from './store/store'
-import router from './router'
-import './fontawesome'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import { store } from './store/store';
 
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+import './fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { router } from "./router";
+
+import config from './config/config';
 
 Vue.config.productionTip = false;
 
-new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app');
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.use(store);
+app.use(router);
+app.use(BootstrapVue3);
+
+// app.config.globalProperties.$config = config;
+
+app.mount('#app');
