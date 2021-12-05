4a899f47aa09a50092f8e26ccc2335a629423129
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-05 10:06:07 +0300

    localize navbar logic

--- code/src/components/navbar/NavBar.vue
+++ code/src/components/navbar/NavBar.vue
@@ -1,30 +1,54 @@
 <template>
     <div>
-      <div>
-        <router-link class="pr-2" :to="'/'">Ablaki</router-link>
+        <router-link class="pe-2" :to="'/'">Ablaki</router-link>
 
-        <router-link class="pr-2" v-for="item in menuTop" :key="item.url" :to="item.url">
-          <font-awesome-icon :icon="item.icon"/>
-          <span class="hidden-sm-down">{{ item.anchor }}</span>
+        <router-link
+            class="pe-2"
+            v-for="item in menuItems"
+            :key="item.url"
+            :to="item.url"
+        >
+            <font-awesome-icon :icon="item.icon" />
+            <span class="hidden-sm-down">{{ item.anchor }}</span>
         </router-link>
-      </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "NavBar",
-        props: {
-          menuTop: {type: Array},
-        },
-        data: function () {
-            return {
+import { mapGetters } from 'vuex';
 
+export default {
+	name: "NavBar",
+	data: function () {
+		return {
+            menuItems: []
+        };
+	},
+    created: function () {
+
+    },
+    computed: {
+        ...mapGetters([
+            'isAuthenticated',
+        ]),
+        menuItems() {
+            let items = [
+                {anchor: 'Форум', url: '/forum', title: 'Форум', icon: 'comments'},
+                {anchor: 'Wiki', url: '/wiki', title: 'wiki', icon: 'question-circle'},
+            ];
+            
+            if (!this.isAuthenticated) {
+                items.push({anchor: 'Регистрация', url: '/users/registration', title: 'Регистрация', icon: 'plus'});
+                items.push({anchor: 'Войти', url: '/users/login', title: 'Авторизация', icon: 'sign-in-alt'});
+            } else {
+                items.push({anchor: 'Выход', url: '/users/logout', title: 'Выход', icon: 'sign-out-alt'});
             }
-        },
+
+            return items;
+        }
     }
+};
 </script>
 
 <style scoped>
-
 </style>
