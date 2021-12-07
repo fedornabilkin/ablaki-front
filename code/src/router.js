c1a61dd59655d02512354b5d3be71fe83cdc55ec
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-07 15:17:19 +0300

    add wall

--- code/src/router.js
+++ code/src/router.js
@@ -16,44 +16,48 @@ import Profile from './components/pages/user/Profile';
 import Ablaki from './components/pages/games/Ablaki';
 import Orel from "./components/pages/games/Orel";
 
+import { store } from './store/store';
+
+
 // 2. Определяем несколько маршрутов
 // Каждый маршрут должен указывать на компонент.
 // "Компонентом" может быть как конструктор компонента, созданный
 // через `Vue.extend()`, так и просто объект с опциями компонента.
 
 const routes = [
-    {
-        path: "/:pathMatch(.*)*'",
-        name: 'Error',
-        component: PageNotFound,
-        meta: {
-            required: true
-        }
-    },
-    {
-        path: '/',
-        component: Main,
-        meta: {
-            required: true
-        }
-    },
+    { path: "/:pathMatch(.*)*'", name: 'Error', component: PageNotFound },
+    { path: '/', component: Main },
 
-    {path: '/forum', component: Forum},
-    {path: '/wiki', component: Wiki},
+    { path: '/forum', component: Forum },
+    { path: '/wiki', component: Wiki },
 
-    {path: '/users/wall/:login', component: Wall},
-    {path: '/users/registration', component: Registration},
-    {path: '/users/login', component: Login},
-    {path: '/users/logout', component: Logout},
-    {path: '/users/profile', component: Profile},
+    { path: '/users/wall/:login', component: Wall, meta: { requiresAuth: true } },
+    { path: '/users/registration', component: Registration },
+    { path: '/users/login', component: Login },
+    { path: '/users/logout', component: Logout },
+    { path: '/users/profile', component: Profile },
 
-    {path: '/games/ablaki', component: Ablaki},
-    {path: '/games/orel', component: Orel},
+    { path: '/games/ablaki', component: Ablaki },
+    { path: '/games/orel', component: Orel },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes: routes,
 });
+
+router.beforeEach((to, from) => {
+    // instead of having to check every route record with
+    // to.matched.some(record => record.meta.requiresAuth)
+    if (to.meta.requiresAuth && store.getters.isAuthenticated === false) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        return {
+            path: '/',
+            // save the location we were at to come back later
+            //query: { redirect: to.fullPath },
+        }
+    }
+})
 
 export { router };
--- code/src/services/api.js
+++ code/src/services/api.js
@@ -3,8 +3,6 @@ import config from "../config/config";
 
 const baseUrl = config.getParam('apiDomain')
 
-console.log(baseUrl);
-
 export const login = async (login, password) => {
     return new Promise((resolve, reject) => {
         axios.post(`${baseUrl}login`, { login, password }).then(res => {
@@ -32,6 +30,18 @@ export const registration = async (username, email, password) => {
 export const logout = async () => {
     return new Promise((resolve, reject) => {
         axios.post(`${baseUrl}logout`, {}).then(res => {
+            if (!res.data.errors) {
+                resolve(res.data)
+            } else {
+                reject(res.data)
+            }
+        }).catch(e => reject(e));
+    });
+}
+
+export const getWall = async (username) => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${baseUrl}v1/users/wall/${username}`).then(res => {
             if (!res.data.errors) {
                 resolve(res.data)
             } else {
