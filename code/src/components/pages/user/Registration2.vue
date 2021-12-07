0e7befdf2455ab37deaa0d825676402f1f1e480a
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-07 04:56:56 +0300

    complete registration

--- code/src/components/pages/user/Registration2.vue
+++ code/src/components/pages/user/Registration2.vue
@@ -14,9 +14,10 @@
 			:rules="rules"
 			label-position="top"
 			class="mt-3"
+			@submit.prevent="submit"
 		>
-			<el-form-item label="Логин" prop="name">
-				<el-input v-model="form.name"></el-input>
+			<el-form-item label="Логин" prop="username">
+				<el-input v-model="form.username"></el-input>
 			</el-form-item>
 			<el-form-item label="Email" prop="email">
 				<el-input v-model="form.email"></el-input>
@@ -32,38 +33,57 @@
 			</el-form-item>
 
 			<el-form-item size="medium">
-				<el-button type="primary" @click="submit">Регистрация</el-button>
+				<el-button type="primary" native-type="submit" :disabled="!btnEnabled">Регистрация</el-button>
 			</el-form-item>
 		</el-form>
 	</div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { ElNotification } from 'element-plus'
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 
 export default {
 	setup() {
+		const store = useStore();
+		const router = useRouter();
+
 		const formRef = ref();
 		const form = ref({
-			name: "",
+			username: "",
 			email: "",
 			password: "",
 			passwordRepeat: "",
 			rules: false,
 		});
-		const rules = ref({
-			name: [
+
+		const errors = {};
+
+		const isLoading = computed(() => store.getters.authStatus === "loading");
+
+		const rules = {
+			username: [
 				{
 					required: true,
 					message: "Введите логин",
 					trigger: "blur",
-				},{
+				}, {
 					min: 3,
 					max: 100,
 					message: "Логин должен быть не менее 3х символов",
 					trigger: "blur",
-				},
+				}, {
+					validator: (rule, value, callback) => {
+						if (errors.username) {
+							callback(new Error(errors?.username));
+							errors.username = "";
+						} else {
+							callback();
+						}
+					}
+				}
 			],
 			email: [
 				{
@@ -71,24 +91,42 @@ export default {
 					type: "email",
 					message: "Введите корректный Email",
 					trigger: "blur",
-				},{
+				}, {
 					message: "Введите Email",
 					trigger: "blur",
-				},
+				}, {
+					validator: (rule, value, callback) => {
+						if (errors.email) {
+							callback(new Error(errors?.email));
+							errors.email = "";
+						} else {
+							callback();
+						}
+					}
+				}
 			],
 			password: [
 				{
 					required: true,
 					message: "Введите пароль",
 					trigger: "blur",
-				},
+				}, {
+					validator: (rule, value, callback) => {
+						if (errors.password) {
+							callback(new Error(errors?.password));
+							errors.password = "";
+						} else {
+							callback();
+						}
+					}
+				}
 			],
 			passwordRepeat: [
 				{
 					required: true,
 					message: "Введите пароль повторно",
 					trigger: "blur",
-				},{
+				}, {
 					validator: (rule, value, callback) => {
 						if (value !== form.value.password) {
 							callback(new Error('Пароли должны совпадать'));
@@ -111,32 +149,54 @@ export default {
 					}
 				}
 			]
-		});
+		};
 
-		const isLoading = ref(false);
+		const isAuthenticated = computed(() => store.getters.isAuthenticated);
+		if (isAuthenticated.value) {
+			router.push('/');
+		}
 
 		const submit = () => {
 			formRef.value.validate(async (valid) => {
 				if (valid === true) {
-					isLoading.value = true;
+					let username = form.value.username;
+					let email = form.value.email;
+					let password = form.value.password;
 
-					setTimeout(() => {
-						isLoading.value = false;
-
+					store.dispatch('registration', {
+						username,
+						email,
+						password,
+					}).then((res) => {
 						ElNotification({
 							title: 'Успешно',
 							message: 'Письмо с подтверждением отправлено на Email',
 							type: 'success',
 						});
-					}, 1500)
+
+						this.$router.push("/");
+					}).catch(e => {
+						for (const errorKey in e.errors) {
+							errors[errorKey] = e.errors[errorKey]
+						}
+
+						console.log("errors", errors);
+						formRef.value.validate();
+					})
+				} else {
+					console.log("invalid");
 				}
 			});
 		};
+
+		const btnEnabled = computed(() => form.value.username && form.value.email && form.value.password && form.value.passwordRepeat && form.value.rules);
+
 		return {
 			formRef,
 			form,
 			rules,
 			isLoading,
+			btnEnabled,
 			submit,
 		};
 	},
