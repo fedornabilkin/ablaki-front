edfbf3c268c851fe3d6678f0e7e2bb72c96f8fd9
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-11 03:53:48 +0300

    add api calls

--- code/src/services/api.js
+++ code/src/services/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import config from "../config/config";
 
-const baseUrl = config.getParam('apiDomain')
+const baseUrl = config.getParam('apiDomain');
 
 export const login = async (login, password) => {
     return new Promise((resolve, reject) => {
         axios.post(`${baseUrl}login`, { login, password }).then(res => {
             if (!res.data.errors) {
-                resolve(res.data)
+                resolve(res.data);
             } else {
-                reject(res.data)
+                reject(res.data);
             }
         }).catch(e => reject(e));
     });
@@ -19,9 +19,9 @@ export const registration = async (username, email, password) => {
     return new Promise((resolve, reject) => {
         axios.post(`${baseUrl}registration`, { username, email, password }).then(res => {
             if (!res.data.errors) {
-                resolve(res.data)
+                resolve(res.data);
             } else {
-                reject(res.data)
+                reject(res.data);
             }
         }).catch(e => reject(e));
     });
@@ -31,9 +31,9 @@ export const logout = async () => {
     return new Promise((resolve, reject) => {
         axios.post(`${baseUrl}logout`, {}).then(res => {
             if (!res.data.errors) {
-                resolve(res.data)
+                resolve(res.data);
             } else {
-                reject(res.data)
+                reject(res.data);
             }
         }).catch(e => reject(e));
     });
@@ -43,10 +43,36 @@ export const getWall = async (username) => {
     return new Promise((resolve, reject) => {
         axios.get(`${baseUrl}v1/users/wall/${username}`).then(res => {
             if (!res.data.errors) {
-                resolve(res.data)
+                resolve(res.data);
             } else {
-                reject(res.data)
+                reject(res.data);
             }
         }).catch(e => reject(e));
     });
+}
+
+export const orel = {
+    get: async () => {
+        return new Promise((resolve, reject) => {
+            axios.get(`${baseUrl}v1/orel`).then(res => {
+                if (!res.data.errors) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data);
+                }
+            }).catch(e => reject(e));
+        });
+    },
+
+    create: async (kon, count) => {
+        return new Promise((resolve, reject) => {
+            axios.post(`${baseUrl}v1/orel`, {kon, count}).then(res => {
+                if (!res.data.errors) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data);
+                }
+            }).catch(e => reject(e));
+        });
+    }
 }
