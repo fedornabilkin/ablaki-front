1768a9c1fcb8a5609cb81f8f65c8ff56eae20971
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-23 23:32:09 +0300

    add tooltip credits change

--- code/src/components/navbar/UserBar.vue
+++ code/src/components/navbar/UserBar.vue
@@ -8,32 +8,67 @@
 		
 		<div class="user-bar-right">
 			<div>
-				<span class="amount">{{ user.person.balance }}</span>
+				<span>{{ user.person.balance }}</span>
 				<span>Кг</span>
 			</div>
 			<div>
-				<span class="amount">{{ user.person.credit }}</span>
-				<span>Cr</span>
+				<el-tooltip
+					effect="dark"
+					:content="creditsTooltipContent"
+					placement="top"
+					v-model="creditsTooltipAnimation"
+					:manual="true"
+				>
+					<div>
+						<span>{{ roundCredits(user.person.credit) }}</span>
+						<span>Cr</span>
+					</div>
+				</el-tooltip>
 			</div>
 			<div>
 				<el-icon style="vertical-align: text-top;"><star /></el-icon>
-				<span class="amount">{{ user.person.rating }}</span>
+				<span>{{ user.person.rating }}</span>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import { computed } from '@vue/reactivity';
+import { computed, ref } from '@vue/reactivity';
 import { useStore } from 'vuex';
+import { watch } from '@vue/runtime-core';
 
 export default {
 	setup() {
 		const store = useStore();
 		const user = computed(() => store.getters['auth/user']);
+		const creditsTooltipAnimation = ref(false);
+		const creditsTooltipContent = ref("");
+
+		const creditsTooltipAnimationTimeout = ref();
+
+		watch(() => user.value.person.credit, (value, oldValue) => {
+			let diff = roundCredits(value - oldValue);
+			let displayedDiff = `${diff < 0 ? '-' : '+'}${Math.abs(diff)}`;
+
+			creditsTooltipContent.value = `${displayedDiff} Cr`
+			creditsTooltipAnimation.value = true;
+
+			clearTimeout(creditsTooltipAnimationTimeout.value);
+			creditsTooltipAnimationTimeout.value = setTimeout(() => {
+				creditsTooltipAnimation.value = false;
+			}, 2500);
+		});
+
+		const roundCredits = (credits) => {
+			return Math.round(credits * 10) / 10;
+		}
 
 		return {
 			user,
+			creditsTooltipAnimation,
+			creditsTooltipContent,
+			roundCredits,
 		}
 	},
 }
