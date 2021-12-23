3520e07e53ac4b7038a50c0bdfa9a29dd245ff96
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-24 00:24:29 +0300

    add error handler

--- code/src/services/api.js
+++ code/src/services/api.js
@@ -138,3 +138,21 @@ export const orel = {
         });
     },
 }
+
+export const errorHandler = (e, errors) => {
+    if (!errors.hasOwnProperty("404")) {
+        errors['404'] = () => alert("something сломалось")
+    }
+    if (!e.response) {
+        console.log("Ошибка Интернета");
+    } else {
+        Object.keys(errors).map(function (key) {
+            let error = String(e.response.data.message);
+            let errorCode = String(e.response.status);
+
+            if (error === key || errorCode === key) {
+                errors[key]();
+            }
+        })
+    }
+}
