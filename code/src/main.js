0230ee6b10aa2c14d97214126896f29c50b60964
Evgeniy Dubenyuk <evgeniidubenyuk87@gmail.com>
2021-12-29 15:46:04 +0300

    add css bs mixins

--- code/src/main.js
+++ code/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 
 // import 'element-plus/dist/index.css';
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue';
 import { router } from './router';
 import { store } from './store/store';
--- code/vue.config.js
+++ code/vue.config.js
@@ -3,7 +3,19 @@ module.exports = {
         proxy: 'http://94.250.251.94:3180/'
     },
     chainWebpack: config => {
-        // config.resolve.alias.set('@', resolve('src'));
         config.resolve.extensions.add('.vue', '.js');
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
+                    @import "@/index.scss";
+                `
+            }
+        }
     }
 }
+
+// @import "~bootstrap/scss/functions";
+// @import "~bootstrap/scss/variables";
+// @import "~bootstrap/scss/mixins";
